99f2933e30c471a6f820ca029e10c3a830552e82
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-05-19 20:52:46 +0300

    Refactor The Code

--- lib/models/category_list.dart
+++ lib/models/category_list.dart
@@ -1,4 +1,4 @@
-import 'package:news_app/models/featured_news_model.dart';
+import 'package:news_app/models/category_model.dart';
 
 const List<CategoryModel> categoryList = [
   CategoryModel(
--- lib/widgets/category_container.dart
+++ lib/widgets/category_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/models/featured_news_model.dart';
+import 'package:news_app/models/category_model.dart';
 import 'package:news_app/views/category_view.dart';
 
 class CategoryContainer extends StatelessWidget {
